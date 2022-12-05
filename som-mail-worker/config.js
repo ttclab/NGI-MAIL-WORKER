1ae25fa5b7eb8b085b428060a24af8b7972f9648
Marco Nanni <marco.nanni@live.it>
2022-12-05 15:42:32 +0100

    up

--- som-mail-worker/config.js
+++ som-mail-worker/config.js
@@ -1,17 +1,17 @@
 // Internal settings
-exports.appname                = "eep-mail-worker";
+exports.appname = "som-mail-worker";
 // AMQP settings
 //exports.amqp_server_addr       = "localhost";
-exports.amqp_server_addr       = "litlnx21";
-exports.amqp_server_port       = 5672;
-exports.amqp_heartbeat         = 60;
-exports.amqp_req_queue         = "eep-mail-queue";
-exports.exchange_name          = "eep";
-exports.routing_key            = "eep-queue-direct";
-exports.msg_timeout            = 30000;
+exports.amqp_server_addr = "litlnx21";
+exports.amqp_server_port = 5672;
+exports.amqp_heartbeat = 60;
+exports.amqp_req_queue = "som-mail-queue";
+exports.exchange_name = "som";
+exports.routing_key = "som-queue-direct";
+exports.msg_timeout = 30000;
 // TEST
-exports.amqp_server_username    = "admin";
-exports.amqp_server_password    = "2Rb!t1Mq";
+exports.amqp_server_username = "admin";
+exports.amqp_server_password = "2Rb!t1Mq";
 // PRODUZIONE
 //exports.amqp_server_username    = "admin";
 //exports.amqp_server_password    = "1Rb!t7Mq";
